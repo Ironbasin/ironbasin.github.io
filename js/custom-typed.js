89dac194f4ca7faf91ce47df740fe24e0c23f997
Ironbasin <ironbasin@outlook.com>
2024-01-21 20:11:20 +0800

    Site updated: 2024-01-21 20:11:19

--- js/custom-typed.js
+++ js/custom-typed.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var typedElement = document.querySelector('#site-title'); // 将选择器替换成存放副标题的元素
+    var typedElement = document.querySelector('#site-title');
     if (typedElement) {
-      var typed = new Typed('.your-element-selector', { // 这应和上面的选择器一致
+      var typed = new Typed(typedElement, {
         strings: ["从现在开始，做一个幸福的人。"],
         typeSpeed: 50,
         loop: true
       });
     }
   });
+  
